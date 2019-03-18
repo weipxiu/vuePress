f8782da8e9b3af5e52e0103638765b1ae39656b4
李俊MacBook <lijun@021.com>
2019-03-19 02:09:18 +0800

    更新demo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,45 @@
 module.exports = {
-  //页面顶部title
-  title: 'Hello VuePress',
-  //页面描述
-  description: 'Just playing around',
+  title: "vuePress_Demo",
+  description: "The description of the site.",
+  head: [["link", { rel: "icon", href: `/logo.png` }]],
+  base: "/",
+  dest: "./dist",
+
   themeConfig: {
-    sidebar: [
-      {
-        title: '入门',
-        children: [
-          '/install/',
-          'get-started/',
-        ]
-      },
-      {
-        title: '组件',
-        children: ['/components/button']
-      }
-    ]
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "单页", link: "/projects/" },
+      { text: "分栏", link: "/guide/" },
+      { text: "Markdown Demo", link: "/article/" },
+      { text: "仓库", link: "https://github.com/weipxiu/vuePress" }
+    ],
+    sidebar: {
+      '/guide/': genSidebarConfig('Guide')
+    },
+    lastUpdated: 'Last Updated'
+  },
+
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    config: md => {
+      md.use(require("markdown-it-katex"));
+    }
   }
+};
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'customize',
+        'advanced',
+      ]
+    }
+  ]
 }
+
